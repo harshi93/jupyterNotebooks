e8d0654f4003f8b543cf9c82b3852aeb6d17313d
Harshit Singh <singhharshit.4674@gmail.com>
2020-02-01 10:33:37 -0800

    learning classes

---
+++ Python-Practice/create-and-instantiate-classes.py
@@ -0,0 +1,29 @@
+#TODO Create Classes
+
+class Employee:
+    # init can be viewed as constructor, we can class instances whatever we want but by convention
+    # we should refer them as self
+    # pass statement - (the statement is used to simply ignore the code block)
+    def __init__(self, first, last, name, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+#TODO Instantiate Classes
+    emp_1 = Employee('Corey', 'Schaefer', 50000) # this is how class objects are instantiated
+    emp_2 = Employee('Test', 'User', 60000)
+
+#TODO Instance Variables
+    # instance variable contain data that is unique to each instance,
+    # the below mentioned is an example of instance variable
+    print(emp_1);
+    print(emp_2;) # these both will have unique addresses
+    print(emp_1.fullname()) # here self is getting passed in automatically
+
+# TODO Class Variables
+    Employee.fullname(emp_1); # because this a class variable we need to explicitly
+    Employee.fullname(emp_2); # pass in the instance
